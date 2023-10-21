913de1e20268e903c6119e5883809ba597654f87
niikolaypopov <148642726+niikolaypopov97@users.noreply.github.com>
2023-10-21 19:40:57 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,6 @@
-# output "cluster_name" {
-#   value = module.eks.cluster_name
-# }
-
-# output "cluster_endpoint" {
-#   value = module.eks.cluster_endpoint
-# }
-
 output "rds_endpoint" {
   value = aws_db_instance.my-db.endpoint
 }
-
-# output "cluster_sec_group" {
-#   value = module.eks.cluster_security_group_id
-# }
 
 output "eks_lb" {
   value = kubernetes_service.wordpress_serv.status
